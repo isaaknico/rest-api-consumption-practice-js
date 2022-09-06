944bb77b786c932fbebc1379c23e3230427594d7
isaaknico <kaasi_96@hotmail.com>
2022-09-05 23:22:39 -0500

    Added Trending Movies from TMDB API

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-console.log('Hello world')
+async function getTrendingMoviesHome () {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
 
-console.log('https://api_url?api_key=' + API_KEY);
+    console.log({ res, data });
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const carousel = document.querySelector('#trendingMovies .carousel');
+
+        const carouselItem = document.createElement('article');
+        carouselItem.classList.add('carousel__item');
+
+        const element = document.createElement('div');
+        element.classList.add('movie');
+
+        const img = document.createElement('img');
+        img.classList.add('movie__img');
+        img.setAttribute('alt', movie.title);
+        img.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        element.appendChild(img);
+        carouselItem.appendChild(element);
+        carousel.appendChild(carouselItem);
+    });
+}
+
+getTrendingMoviesHome();
