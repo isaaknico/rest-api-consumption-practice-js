4fe7516208f8d841ab7f9c2364ffb4e5e020334c
isaaknico <kaasi_96@hotmail.com>
2022-09-06 14:10:33 -0500

    Added Movies Categories from TMDB API

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ async function getTrendingMoviesHome () {
     console.log({ res, data });
 
     const movies = data.results;
-    movies.forEach(movie => {
+    movies.forEach( movie => {
         const carousel = document.querySelector('#trendingMovies .carousel');
 
         const carouselItem = document.createElement('article');
@@ -25,4 +25,30 @@ async function getTrendingMoviesHome () {
     });
 }
 
+async function getCategoriesMoviesHome () {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+
+    console.log({ res, data });
+
+    const categories = data.genres;
+
+    categories.forEach( element => {
+        const carousel = document.querySelector('#categoriesMoviesHome .carousel');
+
+        const carouselItem = document.createElement('li');
+        carouselItem.classList.add('carousel__item');
+
+        const category = document.createElement('a');
+        category.classList.add('category');
+
+        const categoryName = document.createTextNode(element.name);
+        
+        category.appendChild(categoryName);
+        carouselItem.appendChild(category);
+        carousel.appendChild(carouselItem);
+    })
+}
+
 getTrendingMoviesHome();
+getCategoriesMoviesHome();
